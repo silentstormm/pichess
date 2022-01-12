a8e337b426bca1b3837d10f1c15702977d2e4e66
Munio07 <krzysm007@gmail.com>
2022-01-12 14:10:17 +0100

    added routes

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express = require("express");
-const http = require("http");
+const express = require('express')
+const http = require('http')
+const websocket = require('ws')
 
-const port = process.argv[2];
-const app = express();
+const indexRouter = require('./routes/index')
 
-app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
+const port = process.argv[2]
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/play', indexRouter)
+app.get('/', indexRouter)
+
+const server = http.createServer(app)
+const wss = new websocket.Server({ server })
+
+const websockets = {}
+
+wss.on('connection', function connection(ws) {})
+
+server.listen(port)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/play', function (req, res) {
+  res.sendFile('play.html', { root: './public' })
+})
 
-module.exports = router;
+router.get('/', function (req, res) {
+  res.sendFile('index.html', { root: './public' })
+})
+
+module.exports = router
